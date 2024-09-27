bb190f747d6ec0ef2652b5d12efed5779c40c06c
cheer-cho <178830338+cheer-cho@users.noreply.github.com>
2024-09-27 16:06:58 -0500

    [1] - start implementing react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-function App() {
-  return <div></div>;
-}
+import { useQuery } from '@tanstack/react-query';
+import { getUsers } from './api/user';
 
-export default App;
+export default function App() {
+  const { data } = useQuery({
+    queryKey: ['users'],
+    queryFn: () => getUsers(),
+  });
+
+  return (
+    <div>
+      {data?.map((user) => {
+        return <div key={user.id}>{user.name}</div>;
+      })}
+    </div>
+  );
+}
